35a3a1c3b8253aa781cea7ce7d0ff0f3cf318314
Raynard Dodzi <dodzireynard@gmail.com>
2019-11-13 07:27:01 +0000

    update

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
+app_name = "home"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("message/", views.message, name="message"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Message
+
 
 def index(request):
 
     return render(request, "home/index.html")
+
+def message(request):
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    message = request.POST.get("message")
+    message_obj = Message.objects.create(name=name, email=email, message=message)
+    print(name, email, message)
+
+    return redirect("home:index")
