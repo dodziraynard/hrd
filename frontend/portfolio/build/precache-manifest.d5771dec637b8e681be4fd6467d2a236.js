3f6ec00672aa24d7234ae3befc72fff5bef149d4
Raynard Dodzi Helegah <45526474+dodziraynard@users.noreply.github.com>
2020-09-25 01:59:48 +0000

    update

---
+++ frontend/portfolio/build/precache-manifest.d5771dec637b8e681be4fd6467d2a236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6766d6ad17dcd0ffcef3eef88bdf38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e9313cc714bd20e1b29",
+    "url": "/static/js/2.180af951.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.180af951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca35faceeacf68cca5c",
+    "url": "/static/js/main.533633a5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "536768045dde130fe61cc6de241a6e42",
+    "url": "/static/media/bg.53676804.jpg"
+  }
+]);
